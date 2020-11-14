a081990a79d3d2356383731b83ccddf1552ef06f
Kevy Augusto Mendes Granero <kevyaugusto@gmail.com>
2020-11-14 17:06:32 +1100

    Added EventBus and EventHandler interfaces. Also added Command, Event and Message abstract classes into Domain.Core project.

---
+++ MicroservicesRabbitMQ.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,25 @@
+﻿using MicroservicesRabbitMQ.Domain.Core.Commands;
+using MicroservicesRabbitMQ.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroservicesRabbitMQ.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TH">Event Handler</typeparam>
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
